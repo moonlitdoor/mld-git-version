ff110bb8f0240616a89e5d4a867ec09d3deaad3b
Brian Amesbury <bamesbury@sofi.org>
2018-08-21 17:38:10 -0600

    publish to gradle plugins

--- git-version/build.gradle.kts
+++ git-version/build.gradle.kts
@@ -4,19 +4,26 @@ plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
     `maven-publish`
+    id("com.gradle.plugin-publish") version "0.10.0"
 }
 
 gradlePlugin {
     (plugins) {
         "git-version" {
-            id = "git-version"
+            id = "com.moonlitdoor.git-version"
             implementationClass = "com.moonlitdoor.git.version.GitVersionPlugin"
         }
     }
 }
 
-publishing {
-    repositories {
-        //        maven(url = "build/repository")
+pluginBundle {
+    website = "https://www.moonlitdoor.com"
+    vcsUrl = "https://github.com/moonlitdoor/mld-git-version"
+    description = "A Gradle plugin that sets various properties available for use as the version based on the git repository."
+    (plugins) {
+        "git-version" {
+            displayName = "Git Version Plugin"
+            tags = listOf("git", "version", "android", "java")
+        }
     }
 }
